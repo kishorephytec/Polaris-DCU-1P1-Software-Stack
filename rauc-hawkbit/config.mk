dcae26abb9795fbe97afe400a5e8365316ba6299
Srikanth J <srikanth.j@phytecembedded.in>
2024-08-02 11:44:37 +0530

    rauc client-application for multi-files updation on target
    Signed-off-by: Srikanth J <srikanth.j@phytecembedded.in>

---
+++ rauc-hawkbit/config.mk
@@ -0,0 +1,9 @@
+{
+"Image_Name": "update_bundle.tar.gz",
+"Download_Path": "/root",
+"Bin_path": "/root/wirepas",
+"Update_Folder": "/root/update_folder",
+"app_service": "wirepasTransport.service",
+"app_service1": "polarisTransport.service"
+}
+
